0cb71529595feced99dc2e6c3bbc09abc79c785b
Phoenix <kimuraeiji214@gmail.com>
2017-02-07 03:30:08 +0100

    Initial commit & working with AppStore submission
    Signed-off-by: Phoenix <kimuraeiji214@gmail.com>

---
+++ Freeze/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Freeze
+//
+//  Created by SamuelCardo on 24/10/16.
+//  Copyright © 2016 Silvergate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Freeze/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Freeze
+//
+//  Created by SamuelCardo on 24/10/16.
+//  Copyright © 2016 Silvergate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
